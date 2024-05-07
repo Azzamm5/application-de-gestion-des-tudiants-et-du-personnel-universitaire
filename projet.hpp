ea97b33c3b0eecc83060a0b17e2222f42b6a1b4a
Azzamm5 <105621085+Azzamm5@users.noreply.github.com>
2024-05-08 00:26:41 +0100

    Update projet.hpp

--- projet.hpp
+++ projet.hpp
@@ -4,170 +4,318 @@
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
-
-// ************************************| Class etudiant |*******************************************************
-class etudiant{
-protected:
-    string name;
-    string prenom;
-    string matricule;
-    string filliere;
-
-public:
-    etudiant(string na, string pre, string mat, string fill);
-    void affichEtudiant() const;
-    string getMatricule() const ;
-    string getName() const ;
-    string getPrenom() const;
-    string getFilliere() const;
-     void modifierEtudiant(string na, string pre, string mat, string fill);
-
+//************************************| Class Etudiant |*******************************************************
+class Etudiant {
+    private:
+        string  Mat;
+        string  Nom;
+        string  Prenom;
+        string  Niveau;
+        string  Filiere;
+        string  Site;
+        string coursinscrits;
+        string status;
+       
+        // string Password;
+    public:
+    //Etudiant(){};
+        Etudiant(string mat,string nom,string prenom,string niveau,string filiere,string site);
+        string getMat() ;
+        string getNom() ;
+        string getPrenom() ;
+        string getNiveau() ;
+        string getFiliere() ;
+        string getSite();
+        void setFiliere(string F);
+        void setSite(string S);
+        void setMat(string M);
+        string getCoursinscrits();
+        void setCoursinscrits(string C);
+        string getStatus();
+        void setStatus(string s);
+        void CreationEtudiant(Etudiant& E);
+        void AfficherEtudiant() ; 
+        void ModifierEtudiant(string N,string S);
+        void RechercheEtudiant(string M);
+        void SupprimerEtudiant( string& mat);
+        void afficheinscription();
+        
 };
-//**********************************************|**********************************************|************
-
-
+//**************************************************************************************************************
 
 
 
 //************************************| Class Gestion Etudiant |*******************************************************
-class Gestion_etudiant {
-private:
-    vector<etudiant> listeEtudiants;
-
-public:
-    void ajouterEtudiant(const etudiant& e);
-    void supprimerEtudiant(const string& matricule,const string& name);
-    void afficherTousLesEtudiants() const ;
-    void rechercherEtudiantParNom(const string& name,const string& filliere ,const string& matricule);
+class GestionEtudiant{
+    private:
+    vector <Etudiant> Etudiants;
+    public:
+    GestionEtudiant();
+    vector<Etudiant>& getEtudiants();
+    void  AfficherDetailsEtudiant();  
+    void  CreationEtudiant(Etudiant& E);
+    void  ModifierEtudiant();
+    void  RechercheEtudiant(string m);
+    void  SupprimerEtudiant(string mat);
+    Etudiant* rechercherEtudiantParNom(string matricule);
 };
-//**********************************************|**********************************************|************
+
+//*********************************************************************************************************************
 
 
 
 
 
+//*******************************************|->  Class Personnel <-|*******************************************************
 
-//************************************| Class Personnel |*******************************************************
-class Personnel {
-protected:
-    string nom;
-    int numero;
-    string departement;
+class Personnel{
+    protected:
+        string MatE;
+        string Nom;
+        string Prenom;
+        string Dep;
+    public:
+        Personnel(string mate,string nom, string prenom, string dep);
+        string getMatE();
+        string getNom();
+        string getPrenom();
+        string getDep();
+        void setDep(string d);        
+    void AfficherPersonnel();
+    // void CreationPersonnel();
 
-public:
-    Personnel(string n, int num, string dep);
-    void afficherDetailPerso() const;
-    string getNom() const;
-    int getNumero() const;
-    string getDepartement() const;
-    void modifierPerso(string n, int num, string dep);
+
 };
-//**********************************************|**********************************************|************
-
-
-
-
+//***************************************************************************************************************
 
 
 
 //************************************| Class Enseignant |*******************************************************
-class Enseignant : public Personnel {
-private:
-    string specialite;
-
-public:
-    Enseignant(string n, int num, string dep, string spec);
-    void afficher() const;
-    string getSpecialite() const;
-    void modifierEns(string n, int num, string dep, string spec);
+class Enseignant:public Personnel{
+    private:
+    string Matiere;
+    public:
+    Enseignant(string mate,string nom,string prenom,string dep,string mat);
+    string getMatiere();
+    void setMatiere(string m);
+    void AfficherEnseignant();
+    void CreationEnseignant();
+    void SupprimerEnseignant();
 };
 
-//**********************************************|**********************************************|************
-
-
-
-
+//******************************************************************************************************************
 
 
 
 //************************************| Class Administratif |*******************************************************
-class Administratif : public Personnel {
-private:
-    string poste;
-
-public:
-    Administratif(string n, int num, string dep, string post);
-    void afficherAdmin() const;
-    string getPoste() const;
-    void modifierAdmin(string n, int num, string dep, string post);
-};
-//**********************************************|**********************************************|************
-
-
-
-
-
-
-//************************************| Class Gestion Personnel |*******************************************************
-class Gestion_personnel{
+class Administratif:public Personnel{
     private:
-        vector<Personnel> listePersonnel;
+        string Poste;
+    public:
+    Administratif(string mate, string nom, string prenom,  string dep, string poste);
+        string getPoste(); 
+        void setPoste(string p);   
+    void AfficherAdministratif();
+    void CreationAdministratif();
+    void ModifierAdministratif();
+    void SupperssionAdministratif();
     
-    public:
-        void ajouterPersonnel(const Personnel& p);
-        void afficherTousLesPersonnel() const;
-        void ajouterEnseigant(const Enseignant& e);
-        void ajouterAdministratif(const Administratif& a);
 };
-//**********************************************|**********************************************|************
+
+//****************************************************************************************************************
 
 
 
-
-
-//************************************| Class Gours |*******************************************************
-
-class cours {
+//************************************| Class Gestion Personnel |*******************************************
+class GestionPersonnel{
     private:
-        string nom_cours;
-        int code_cours;
-        string departement;
-        string niveau_etude;
-        Enseignant* Enseignant_associe;
+        vector<Enseignant> ListEnsignant;
+        vector<Administratif> ListAdministratif;
     public:
-        cours(string n, int code, string dep,string niveau, Enseignant* enseignant);
-        string getNom_cours();
-        void setNom_cours(string s);
-        void setDepartement(string d);
-        int getCode_cours();
-        string getDepartement();
-        string getNiveau_etude();
-        void setNiveau_etude(string niv);
-        void affichecours() const;
-
+        GestionPersonnel();
+        vector<Enseignant>& getListEnsignant();
+        void CreationEnseignant(Enseignant& E);
+        void CreationAdministratif(Administratif& A);
+        void SupprimerEnseignant(string m);
+        void SupprimerAdministratif(string m);
+        void AfficherTousLesEnseignant();
+        void AfficherTousLesAdmin();
+        void RechercheEnseignant(string mate );
+        void RechercheAdministratif(string mate );
+        void ModifierAdmin();
+        void ModifierEnseigant();
 };
-//**********************************************|**********************************************|************
+
+//***************************************************************************************************************
 
 
 
-//************************************| Class Gestion Cours |*******************************************************
+//************************************| Class Cours |*******************************************************
 
-class gestion_cours{
+class Cours {
     private:
-        vector<cours>liste_cours;
+        int IdCours;
+        string NomCours;
+        string FiliereCours;
+        string NiveauCours;
+        string heure;
+        string jour;
     public:
-        void ajouter_cours(const cours& c);
-        void supprimerCours(const int code_cours);
-        void afficherTousCours() const;
-        void modifierCours(const int code_cours, const string& nouveau_nom,const string& nouveau_departement,const string& niv_etude);
-        void consulter_cours(const string& departement, const string& niveau) ;
-
+        Cours(int i,string nom,string f,string n);        
+        int getIdCours();
+        string getHeure();
+        string getJour();
+        void setHeure(string h);
+        void setJour(string j);
+        string getNomCours();
+        string getFilierCours();
+        string getNiveauCours(); 
+        void setNiveauCours(string n);   
+        void AfficherCours();
 };
-//**********************************************|**********************************************|************
+
+//****************************************************************************************************************
+
+
+//************************************| Class Gestion Cours |*****************************************************
+class Gestion_Cours{
+    vector<Cours> ListeDesCours;
+    Enseignant* EnseignantCours;
+    public :
+        vector<Cours>& getListeDesCours();
+        void CreationCours(Cours& c);
+        void recherchercours(string nom);
+        void ConsulterCours(string d,string n);
+        void ModifierCours(string cours);
+        void attribuerenseignant(int id,Enseignant* ens);
+        void affichertouslescours();
+        void SupprimerCours(int i);
+        Enseignant* getEnseignantCours();
+        Cours* rechercherCoursParNom(string nom);
+        
+};
+//****************************************************************************************************************
 
 
 
+//************************************| Class Note |*****************************************************
+class Note{
+    private:
+        Etudiant& Nometudiant;
+        Cours& Nomcours;
+        double note;
+    public:
+        Note(Etudiant& etudiant,Cours& nomcours,double N);
+        Etudiant& getNometudiant();
+        Cours& getNomcours();
+        double getNote();  
+        void setNote(double n);
+        void afficheNote();
+};
+//****************************************************************************************************************
+
+
+
+
+
+//************************************| Class Gestion Note|*****************************************************
+class Gestion_Notes{
+private:
+    vector<Note> ListNote;
+public:
+    Gestion_Notes();
+    void CreationNote(Note& n);
+    void ModifierNote(double NouvelleNote);
+    void ConsulterlesNote();
+};
+
+//****************************************************************************************************************
+
+
+
+
+//************************************| Class Absence |*****************************************************
+
+class Absence {
+private:
+    Etudiant* etudiant;
+    Cours* cours;
+    string Date;
+    string Heure;
+    string motif;
+public:
+    Absence(Etudiant* e, Cours* c, string d,string h);
+    Etudiant* getEtudiant();
+    Cours* getCours();
+    string getDate() ;
+    void setDate(string d);
+    string getHeure();
+    void setHeure(string h);
+    void setMotif(string m);
+    string getMotif();
+    void afficherAbsence();
+    //~Absence();
+};
+//****************************************************************************************************************
+
+
+
+//************************************| Class Gestion Absence |*****************************************************
+
+class GestionAbsence {
+private:
+    vector<Absence> Listabsences;
+    
+public:
+    GestionAbsence();
+    void creerAbsence(Etudiant* etudiant, Cours* cours, string date,string heure);     
+    //void supprimerAbsence(Absence& a);
+    void Abscenceetudiant();
+
+    void modifierStatutAbsenceParMatricule(string matricule, string newStatus,string date);
+};
+
+
+
+
+
+
+class EmploiDuTemps{
+    private:
+        Gestion_Cours& cours;
+        GestionPersonnel& enseignant;
+        string jour;
+        string horaire;
+        
+    public:
+        EmploiDuTemps(Gestion_Cours& c,GestionPersonnel& ens);
+        //void ajouterCours(Cours c);
+        string getJour();
+        string getHoraire();
+        void setJour(string nouveauJour);
+        void setHoraire(string nouvelHoraire);
+        void afficherEmploiDuTemps();
+        void remplirEmploi();
+        void modifierEmploi();
+};
+
+class Gestion_Emploi_du_emps{
+    private: 
+        vector<EmploiDuTemps> Emploi;
+};
+
+
+
+
+void MENU_PRINCIPALE();
+void MENU_GESTION_ETUDIANT();
+void MENU_GESTION_DES_ENSEIGNANTS();
+void MENU_GESTION_DU_PERSONNEL_ADMINISTRATIF();
+void MENU_GESTION_DES_COURS();
+void MENU_GESTION_DES_NOTES();
+void INSCRIPTION();
 
 
 #endif
